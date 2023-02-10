b985477d495f9b51878e2204af118e568ae8cd89
Bahar Eshghi <124599947+bahareshghi2007@users.noreply.github.com>
2023-02-10 11:33:53 +0330

    delete Todo

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const addTodoBtn = document.querySelector('.add-todo-btn');
 const todosContainer = document.querySelector('.todos-list');
 const addTodoInput = document.querySelector('.add-todo-input');
-let result = '';
 
 addTodoBtn.addEventListener('click', (e) => App.addTodo(e.target));
 
@@ -15,38 +14,65 @@ document.addEventListener('DOMContentLoaded', () => {
 class App {
   static addTodo(todo) {
     if (todo.parentElement.classList.contains('add-todo-btn')) {
-      this.createTodo(todo.parentElement.nextElementSibling.value);
+      this.createTodo([
+        todo.parentElement.nextElementSibling.value,
+        new Date().getTime(),
+      ]);
+
       // update LocalStorage:
-      Storage.saveTodo(todo.parentElement.nextElementSibling.value);
+      let todos = Storage.getTodos();
+      todos = [
+        ...todos,
+        [todo.parentElement.nextElementSibling.value, new Date().getTime()],
+      ];
+      Storage.saveTodo(todos);
     } else {
-      this.createTodo(todo.nextElementSibling.value);
+      this.createTodo([todo.nextElementSibling.value, new Date().getTime()]);
+
       // update LocalStorage:
-      Storage.saveTodo(todo.nextElementSibling.value);
+      let todos = Storage.getTodos();
+      todos = [...todos, [todo.nextElementSibling.value, new Date().getTime()]];
+      Storage.saveTodo(todos);
     }
     // reset app:
     addTodoInput.value = '';
   }
 
   static createTodo(todo) {
-    result += `
-        <li class="todo">
-        <input type="text" class="todo-title" value='${todo}'/>
-        <div class="todo-desc">
-        <i class="fa-regular fa-square"></i>
-        <i class="fa-solid fa-trash"></i>
-        </div>
-        </li>
-        `;
-    todosContainer.innerHTML = result;
+    const todoContainer = document.createElement('li');
+    todoContainer.classList.add('todo');
+    todoContainer.innerHTML = `
+     <input type="text" class="todo-title" value='${todo[0]}'/>
+     <div class="todo-desc" data-id ='${todo[1]}'>
+     <i class="fa-regular fa-square"></i>
+     <i class="fa-solid fa-trash"></i>
+     </div>
+    `;
+    todosContainer.appendChild(todoContainer);
+
+    const deleteBtns = document.querySelectorAll('.fa-trash');
+    deleteBtns.forEach((btn) =>
+      btn.addEventListener('click', (e) => this.deleteTodo(e.target))
+    );
+  }
+
+  static deleteTodo(todo) {
+    // update DOM:
+    const deletedTodo = todo.parentElement.parentElement;
+    deletedTodo.remove();
+
+    // update LocalStorage:
+    const todos = Storage.getTodos();
+    const remainingTodos = todos.filter(
+      (t) => t[1] != todo.parentElement.dataset.id
+    );
+    Storage.saveTodo(remainingTodos);
   }
 }
 
 class Storage {
-  static saveTodo(todoToSave) {
-    console.log(todoToSave);
-    let todos = this.getTodos();
-    todos = [...todos, todoToSave];
-    localStorage.setItem('todos', JSON.stringify(todos));
+  static saveTodo(todosToSave) {
+    localStorage.setItem('todos', JSON.stringify(todosToSave));
   }
 
   static getTodos() {
