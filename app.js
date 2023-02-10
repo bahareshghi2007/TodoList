d43d0ef056cb01022f8786e705445f51743e83d6
Bahar Eshghi <124599947+bahareshghi2007@users.noreply.github.com>
2023-02-10 15:06:17 +0330

    check Todo

--- app.js
+++ app.js
@@ -13,25 +13,34 @@ document.addEventListener('DOMContentLoaded', () => {
 
 class App {
   static addTodo(todo) {
+    const id = new Date().getTime();
     if (todo.parentElement.classList.contains('add-todo-btn')) {
-      this.createTodo([
-        todo.parentElement.nextElementSibling.value,
-        new Date().getTime(),
-      ]);
+      this.createTodo([todo.parentElement.nextElementSibling.value, id]);
 
       // update LocalStorage:
       let todos = Storage.getTodos();
       todos = [
         ...todos,
-        [todo.parentElement.nextElementSibling.value, new Date().getTime()],
+        {
+          completed: false,
+          title: todo.parentElement.nextElementSibling.value,
+          id: new Date().getTime(),
+        },
       ];
       Storage.saveTodo(todos);
     } else {
-      this.createTodo([todo.nextElementSibling.value, new Date().getTime()]);
+      this.createTodo([todo.nextElementSibling.value, id]);
 
       // update LocalStorage:
       let todos = Storage.getTodos();
-      todos = [...todos, [todo.nextElementSibling.value, new Date().getTime()]];
+      todos = [
+        ...todos,
+        {
+          completed: false,
+          title: todo.nextElementSibling.value,
+          id: id,
+        },
+      ];
       Storage.saveTodo(todos);
     }
     // reset app:
@@ -41,32 +50,67 @@ class App {
   static createTodo(todo) {
     const todoContainer = document.createElement('li');
     todoContainer.classList.add('todo');
-    todoContainer.innerHTML = `
-     <input type="text" class="todo-title" value='${todo[0]}'/>
-     <div class="todo-desc" data-id ='${todo[1]}'>
-     <i class="fa-regular fa-square"></i>
-     <i class="fa-solid fa-trash"></i>
-     </div>
-    `;
+    if (todo.completed == true) {
+      todoContainer.innerHTML = `
+      <input type="text" class="todo-title complete" value='${
+        todo.title || todo[0]
+      }'/>
+      <div class="todo-desc" data-id ='${todo.id || todo[1]}'>
+      <i class="fa-regular fa-check-square completed"></i>
+      <i class="fa-solid fa-trash"></i>
+      </div>
+     `;
+    } else {
+      todoContainer.innerHTML = `
+       <input type="text" class="todo-title" value='${todo.title || todo[0]}'/>
+       <div class="todo-desc" data-id ='${todo.id || todo[1]}'>
+       <i class="fa-regular fa-square"></i>
+       <i class="fa-solid fa-trash"></i>
+       </div>
+      `;
+    }
     todosContainer.appendChild(todoContainer);
-
-    const deleteBtns = document.querySelectorAll('.fa-trash');
-    deleteBtns.forEach((btn) =>
-      btn.addEventListener('click', (e) => this.deleteTodo(e.target))
-    );
+    todoContainer.addEventListener('click', (e) => this.todoLogic(e.target));
   }
 
-  static deleteTodo(todo) {
-    // update DOM:
-    const deletedTodo = todo.parentElement.parentElement;
-    deletedTodo.remove();
+  static todoLogic(todo) {
+    if (todo.classList.contains('fa-trash')) {
+      // update DOM:
+      const deletedTodo = todo.parentElement.parentElement;
+      deletedTodo.remove();
 
-    // update LocalStorage:
-    const todos = Storage.getTodos();
-    const remainingTodos = todos.filter(
-      (t) => t[1] != todo.parentElement.dataset.id
-    );
-    Storage.saveTodo(remainingTodos);
+      // update LocalStorage:
+      const todos = Storage.getTodos();
+      const remainingTodos = todos.filter(
+        (t) => t.id != todo.parentElement.dataset.id
+      );
+      Storage.saveTodo(remainingTodos);
+    } else if (
+      todo.classList.contains('fa-square') ||
+      todo.classList.contains('fa-check-square')
+    ) {
+      todo.classList.toggle('completed');
+      // update DOM:
+      let todos = Storage.getTodos();
+      let selectedTodo = todos.find(
+        (t) => t.id == todo.parentElement.dataset.id
+      );
+      // todos.forEach((t) => console.log(t.id, todo.parentElement.dataset.id));
+      if (selectedTodo.completed) {
+        selectedTodo.completed = false;
+        todo.parentElement.previousElementSibling.classList.remove('complete');
+        todo.classList.remove('fa-check-square');
+        todo.classList.add('fa-square');
+      } else {
+        selectedTodo.completed = true;
+        todo.parentElement.previousElementSibling.classList.add('complete');
+        todo.classList.remove('fa-square');
+        todo.classList.add('fa-check-square');
+      }
+
+      // update LocalStorage:
+      Storage.saveTodo(todos);
+    }
   }
 }
 
