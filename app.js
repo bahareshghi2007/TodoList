c7daa4722bf5e104e791ce2a030ec6adcde4263a
Bahar Eshghi <124599947+bahareshghi2007@users.noreply.github.com>
2023-02-10 17:51:05 +0330

    search & sort Todos

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const addTodoBtn = document.querySelector('.add-todo-btn');
 const todosContainer = document.querySelector('.todos-list');
 const addTodoInput = document.querySelector('.add-todo-input');
+const searchInput = document.querySelector('.search-input');
+const sortTodos = document.querySelector('.sort');
 
 addTodoBtn.addEventListener('click', (e) => App.addTodo(e.target));
+searchInput.addEventListener('input', (e) => App.searchTodos(e.target));
+sortTodos.addEventListener('click', (e) => App.sortTodos(e.target));
 
 document.addEventListener('DOMContentLoaded', () => {
   const todos = Storage.getTodos();
   todos.forEach((todo) => {
     App.createTodo(todo);
   });
+  // reset App:
+  App.resetApp();
 });
 
 class App {
@@ -81,10 +87,10 @@ class App {
 
       // update LocalStorage:
       const todos = Storage.getTodos();
-      const remainingTodos = todos.filter(
+      const filteredTodos = todos.filter(
         (t) => t.id != todo.parentElement.dataset.id
       );
-      Storage.saveTodo(remainingTodos);
+      Storage.saveTodo(filteredTodos);
     } else if (
       todo.classList.contains('fa-square') ||
       todo.classList.contains('fa-check-square')
@@ -120,6 +126,107 @@ class App {
         Storage.saveTodo(todos);
       });
     }
+  }
+
+  static searchTodos(e) {
+    const todos = Storage.getTodos();
+    const filteredTodos = todos.filter((todo) => todo.title.includes(e.value));
+    let result = '';
+    filteredTodos.forEach((t) => {
+      if (t.completed == true) {
+        result += `
+        <li class='todo'>
+        <input type="text" class="todo-title complete" value='${t.title}'/>
+        <div class="todo-desc" data-id ='${t.id}'>
+        <i class="fa-regular fa-check-square completed"></i>
+        <i class="fa-solid fa-trash"></i>
+        </div>
+        </li>
+       `;
+      } else {
+        result += `
+        <li class='todo'>
+         <input type="text" class="todo-title" value='${t.title}'/>
+         <div class="todo-desc" data-id ='${t.id}'>
+         <i class="fa-regular fa-square"></i>
+         <i class="fa-solid fa-trash"></i>
+         </div>
+         </li>
+        `;
+      }
+    });
+    // update DOM:
+    todosContainer.innerHTML = result;
+  }
+
+  static sortTodos(e) {
+    let result = '';
+    const todos = Storage.getTodos();
+    if (e.value == 'All') {
+      todos.forEach((t) => {
+        if (t.completed == true) {
+          result += `
+          <li class='todo'>
+          <input type="text" class="todo-title complete" value='${t.title}'/>
+          <div class="todo-desc" data-id ='${t.id}'>
+          <i class="fa-regular fa-check-square completed"></i>
+          <i class="fa-solid fa-trash"></i>
+          </div>
+          </li>
+         `;
+        } else {
+          result += `
+          <li class='todo'>
+           <input type="text" class="todo-title" value='${t.title}'/>
+           <div class="todo-desc" data-id ='${t.id}'>
+           <i class="fa-regular fa-square"></i>
+           <i class="fa-solid fa-trash"></i>
+           </div>
+           </li>
+          `;
+        }
+      });
+      // update DOM:
+      todosContainer.innerHTML = result;
+    } else if (e.value == 'Completed') {
+      todos.forEach((t) => {
+        if (t.completed == true) {
+          result += `
+            <li class='todo'>
+            <input type="text" class="todo-title complete" value='${t.title}'/>
+            <div class="todo-desc" data-id ='${t.id}'>
+            <i class="fa-regular fa-check-square completed"></i>
+            <i class="fa-solid fa-trash"></i>
+            </div>
+            </li>
+           `;
+        } else return;
+      });
+      // update DOM:
+      todosContainer.innerHTML = result;
+    } else if (e.value == 'Uncompleted') {
+      todos.forEach((t) => {
+        if (t.completed == false) {
+          result += `
+           <li class='todo'>
+           <input type="text" class="todo-title" value='${t.title}'/>
+           <div class="todo-desc" data-id ='${t.id}'>
+           <i class="fa-regular fa-square"></i>
+           <i class="fa-solid fa-trash"></i>
+           </div>
+           </li>
+           `;
+        } else return;
+      });
+      // update DOM:
+      todosContainer.innerHTML = result;
+    }
+  }
+
+  static resetApp() {
+    addTodoInput.value = '';
+    searchInput.value = '';
+    sortTodos.getElementsByTagName('option')[0].selected = 'selected';
   }
 }
 
